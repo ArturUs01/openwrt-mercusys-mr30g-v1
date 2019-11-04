voyager717/openwrt

commit 3b78161d26524759e393f02005490762285ffe7f

ramips/rt288x,rt3883: Name DTS files based on scheme
Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

target/linux/ramips/image/rt3883.mk
@@ -6,7 +6,7 @@ define Build/mkrtn56uimg
 endef
 
 define Device/edimax_br-6475nd
-  DTS := BR-6475ND
+  MTK_SOC := rt3883
   BLOCKSIZE := 64k
   IMAGE_SIZE := 7744k
   IMAGE/sysupgrade.bin := append-kernel | append-rootfs | \
@@ -21,7 +21,7 @@ TARGET_DEVICES += edimax_br-6475nd
 
 define Device/samsung_cy-swr1100
   $(Device/seama)
-  DTS := CY-SWR1100
+  MTK_SOC := rt3883
   BLOCKSIZE := 64k
   KERNEL := $(KERNEL_DTB)
   SEAMA_SIGNATURE := wrgnd10_samsung_ss815
@@ -35,7 +35,7 @@ TARGET_DEVICES += samsung_cy-swr1100
 
 define Device/dlink_dir-645
   $(Device/seama)
-  DTS := DIR-645
+  MTK_SOC := rt3883
   BLOCKSIZE := 4k
   KERNEL := $(KERNEL_DTB)
   SEAMA_SIGNATURE := wrgn39_dlob.hans_dir645
@@ -48,7 +48,7 @@ TARGET_DEVICES += dlink_dir-645
 
 
 define Device/belkin_f9k1109v1
-  DTS := F9K1109V1
+  MTK_SOC := rt3883
   BLOCKSIZE := 64k
   DEVICE_VENDOR := Belkin
   DEVICE_MODEL := F9K1109
@@ -62,7 +62,7 @@ endef
 TARGET_DEVICES += belkin_f9k1109v1
 
 define Device/omnima_hpm
-  DTS := HPM
+  MTK_SOC := rt3883
   BLOCKSIZE := 64k
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := Omnima
@@ -74,7 +74,7 @@ TARGET_DEVICES += omnima_hpm
 
 
 define Device/asus_rt-n56u
-  DTS := RT-N56U
+  MTK_SOC := rt3883
   BLOCKSIZE := 64k
   IMAGE/sysupgrade.bin += | mkrtn56uimg -s
   DEVICE_VENDOR := Asus
@@ -86,7 +86,7 @@ TARGET_DEVICES += asus_rt-n56u
 
 
 define Device/trendnet_tew-691gr
-  DTS := TEW-691GR
+  MTK_SOC := rt3883
   BLOCKSIZE := 64k
   IMAGES += factory.bin
   IMAGE/factory.bin := $$(sysupgrade_bin) | check-size $$$$(IMAGE_SIZE) | \
@@ -100,7 +100,7 @@ TARGET_DEVICES += trendnet_tew-691gr
 
 
 define Device/trendnet_tew-692gr
-  DTS := TEW-692GR
+  MTK_SOC := rt3883
   BLOCKSIZE := 64k
   IMAGES += factory.bin
   IMAGE/factory.bin := $$(sysupgrade_bin) | check-size $$$$(IMAGE_SIZE) | \
@@ -114,7 +114,7 @@ TARGET_DEVICES += trendnet_tew-692gr
 
 
 define Device/sitecom_wlr-6000
-  DTS := WLR-6000
+  MTK_SOC := rt3883
   BLOCKSIZE := 4k
   IMAGE_SIZE := 7244k
   IMAGES += factory.dlf
@@ -129,7 +129,7 @@ TARGET_DEVICES += sitecom_wlr-6000
 
 
 define Device/loewe_wmdr-143n
-  DTS := WMDR-143N
+  MTK_SOC := rt3883
   BLOCKSIZE := 64k
   DEVICE_VENDOR := Loewe
   DEVICE_MODEL := WMDR-143N

target/linux/ramips/base-files/etc/uci-defaults/09_fix-checksum
@@ -14,9 +14,9 @@ fix_checksum() {
 board=$(board_name)
 
 case "$board" in
-cy-swr1100 | \
+samsung,cy-swr1100|\
 dlink,dch-m225|\
-dir-645 | \
+dlink,dir-645|\
 dlink,dir-860l-b1)
 	fix_checksum seama
 	;;

ArturUs01/openwrt-mercusys-mr30g-v1

commit 34976f6364290098714e4a18d3c3bfee0292f715

ramips: fix MTK_SOC for RT3662 devices
rt3883.mk contains both RT3662 and RT3883 device profiles, but commit
3b78161d2652 set MTK_SOC to rt3883 for all devices. This patch fixes it,
and renames dts files accordingly. And SoC compatible strings are also
appended in the dts.

Fixes: 3b78161d2652 ("ramips/rt288x,rt3883: Name DTS files based on scheme")

Signed-off-by: Sungbo Eo <mans0n@gorani.run>

target/linux/ramips/dts/rt3662_asus_rt-n56u.dts
@@ -6,7 +6,7 @@
 #include <dt-bindings/input/input.h>
 
 / {
-	compatible = "asus,rt-n56u", "ralink,rt3883-soc";
+	compatible = "asus,rt-n56u", "ralink,rt3662-soc", "ralink,rt3883-soc";
 	model = "Asus RT-N56U";
 
 	aliases {

target/linux/ramips/dts/rt3662_dlink_dir-645.dts
@@ -6,7 +6,7 @@
 #include <dt-bindings/input/input.h>
 
 / {
-	compatible = "dlink,dir-645", "ralink,rt3883-soc";
+	compatible = "dlink,dir-645", "ralink,rt3662-soc", "ralink,rt3883-soc";
 	model = "D-Link DIR-645";
 
 	aliases {

target/linux/ramips/dts/rt3662_edimax_br-6475nd.dts
@@ -6,7 +6,7 @@
 #include <dt-bindings/input/input.h>
 
 / {
-	compatible = "edimax,br-6475nd", "ralink,rt3883-soc";
+	compatible = "edimax,br-6475nd", "ralink,rt3662-soc", "ralink,rt3883-soc";
 	model = "Edimax BR-6475nD";
 
 	aliases {

target/linux/ramips/dts/rt3662_loewe_wmdr-143n.dts
@@ -3,7 +3,7 @@
 #include "rt3883.dtsi"
 
 / {
-	compatible = "loewe,wmdr-143n", "ralink,rt3883-soc";
+	compatible = "loewe,wmdr-143n", "ralink,rt3662-soc", "ralink,rt3883-soc";
 	model = "Loewe WMDR-143N";
 };
 

target/linux/ramips/dts/rt3662_omnima_hpm.dts
@@ -6,7 +6,7 @@
 #include <dt-bindings/input/input.h>
 
 / {
-	compatible = "omnima,hpm", "ralink,rt3883-soc";
+	compatible = "omnima,hpm", "ralink,rt3662-soc", "ralink,rt3883-soc";
 	model = "Omnima HPM";
 
 	aliases {

target/linux/ramips/dts/rt3662_samsung_cy-swr1100.dts
@@ -6,7 +6,7 @@
 #include <dt-bindings/input/input.h>
 
 / {
-	compatible = "samsung,cy-swr1100", "ralink,rt3883-soc";
+	compatible = "samsung,cy-swr1100", "ralink,rt3662-soc", "ralink,rt3883-soc";
 	model = "Samsung CY-SWR1100";
 
 	aliases {

target/linux/ramips/image/rt3883.mk
@@ -6,7 +6,7 @@ define Build/mkrtn56uimg
 endef
 
 define Device/asus_rt-n56u
-  MTK_SOC := rt3883
+  MTK_SOC := rt3662
   BLOCKSIZE := 64k
   IMAGE_SIZE := 7872k
   IMAGE/sysupgrade.bin += | mkrtn56uimg -s
@@ -33,7 +33,7 @@ TARGET_DEVICES += belkin_f9k1109v1
 
 define Device/dlink_dir-645
   $(Device/seama)
-  MTK_SOC := rt3883
+  MTK_SOC := rt3662
   BLOCKSIZE := 4k
   IMAGE_SIZE := 7872k
   KERNEL := $(KERNEL_DTB)
@@ -46,7 +46,7 @@ endef
 TARGET_DEVICES += dlink_dir-645
 
 define Device/edimax_br-6475nd
-  MTK_SOC := rt3883
+  MTK_SOC := rt3662
   BLOCKSIZE := 64k
   IMAGE_SIZE := 7744k
   IMAGE/sysupgrade.bin := append-kernel | append-rootfs | \
@@ -60,7 +60,7 @@ endef
 TARGET_DEVICES += edimax_br-6475nd
 
 define Device/loewe_wmdr-143n
-  MTK_SOC := rt3883
+  MTK_SOC := rt3662
   BLOCKSIZE := 64k
   IMAGE_SIZE := 7872k
   DEVICE_VENDOR := Loewe
@@ -70,7 +70,7 @@ endef
 TARGET_DEVICES += loewe_wmdr-143n
 
 define Device/omnima_hpm
-  MTK_SOC := rt3883
+  MTK_SOC := rt3662
   BLOCKSIZE := 64k
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := Omnima
@@ -82,7 +82,7 @@ TARGET_DEVICES += omnima_hpm
 
 define Device/samsung_cy-swr1100
   $(Device/seama)
-  MTK_SOC := rt3883
+  MTK_SOC := rt3662
   BLOCKSIZE := 64k
   IMAGE_SIZE := 7872k
   KERNEL := $(KERNEL_DTB)