ArturUs01/openwrt-mercusys-mr30g-v1

commit 087483fc371c606e62e6f88bec3fb7d20dcac2b5

kernel: netdev: add qlcnic
Add driver for QLogic QLE8240 and QLE8242 Converged Ethernet devices.

Signed-off-by: Vieno Hakkerinen <vieno@hakkerinen.eu>

package/kernel/linux/modules/netdevices.mk
@@ -1193,6 +1193,23 @@ endef
 $(eval $(call KernelPackage,mlx5-core))
 
 
+define KernelPackage/qlcnic
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  DEPENDS:=@PCI_SUPPORT
+  TITLE:=QLogic QLE8240 and QLE8242 device support
+  KCONFIG:=CONFIG_QLCNIC
+  FILES:=$(LINUX_DIR)/drivers/net/ethernet/qlogic/qlcnic/qlcnic.ko
+  AUTOLOAD:=$(call AutoProbe,qlcnic)
+endef
+
+define KernelPackage/macvlan/description
+  This driver supports QLogic QLE8240 and QLE8242 Converged Ethernet
+  devices.
+endef
+
+$(eval $(call KernelPackage,qlcnic))
+
+
 define KernelPackage/sfp
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=SFP cage support

commit 1167d23aaaf7f825806531dafe8c2d85e01c3900

kernel: qlcnic: fix typo in module description
Fixes: 087483fc371c ("kernel: netdev: add qlcnic")
Signed-off-by: Chukun Pan <amadeus@jmu.edu.cn>

package/kernel/linux/modules/netdevices.mk
@@ -1204,7 +1204,7 @@ define KernelPackage/qlcnic
   AUTOLOAD:=$(call AutoProbe,qlcnic)
 endef
 
-define KernelPackage/macvlan/description
+define KernelPackage/qlcnic/description
   This driver supports QLogic QLE8240 and QLE8242 Converged Ethernet
   devices.
 endef