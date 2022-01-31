ArturUs01/openwrt-mercusys-mr30g-v1

commit aa73243715afd4adf5b4b0472b93199cd64cf162

mediatek: use CONFIG_TARGET_ROOTFS_PARTSIZE
Enable 'rootfs-part' feature to make the size of the partition of the
production image configurable instead of hard-coding it.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/mediatek/image/mt7623.mk
@@ -58,10 +58,10 @@ define Build/mt7623-mbr
 			-t 0x41	-N uboot	-p 3584k@320k \
 			-t 0xea	-N recovery	-p 40M@4M \
 		$(if $(findstring sdmmc,$1), \
-			-t 0x2e -N production	-p 208M@48M \
+			-t 0x2e -N production	-p $(CONFIG_TARGET_ROOTFS_PARTSIZE)M@48M \
 		) \
 		$(if $(findstring emmc,$1), \
-			-t 0x2e -N production	-p 976M@48M \
+			-t 0x2e -N production	-p $(CONFIG_TARGET_ROOTFS_PARTSIZE)M@48M \
 		)
 
 	echo -en \

target/linux/mediatek/Makefile
@@ -6,7 +6,7 @@ ARCH:=arm
 BOARD:=mediatek
 BOARDNAME:=MediaTek Ralink ARM
 SUBTARGETS:=mt7622 mt7623 mt7629
-FEATURES:=dt-overlay emmc fpu gpio nand pci pcie separate_ramdisk squashfs usb
+FEATURES:=dt-overlay emmc fpu gpio nand pci pcie rootfs-part separate_ramdisk squashfs usb
 
 KERNEL_PATCHVER:=5.10
 

target/linux/mediatek/image/mt7622.mk
@@ -48,10 +48,10 @@ define Build/mt7622-gpt
 				-N recovery	-r	-p 32M@6M \
 		$(if $(findstring sdmmc,$1), \
 				-N install	-r	-p 7M@38M \
-			-t 0x2e -N production		-p 211M@45M \
+			-t 0x2e -N production		-p $(CONFIG_TARGET_ROOTFS_PARTSIZE)M@45M \
 		) \
 		$(if $(findstring emmc,$1), \
-			-t 0x2e -N production		-p 980M@40M \
+			-t 0x2e -N production		-p $(CONFIG_TARGET_ROOTFS_PARTSIZE)M@40M \
 		)
 	cat $@.tmp >> $@
 	rm $@.tmp

commit 28f22b03e3e5a0042694dfcb590878a11b699d34

mediatek: mt7623: simplify partition generation
The two options 'emmc' and 'sdmmc' now became identical lines after
introducing CONFIG_TARGET_ROOTFS_PARTSIZE.
Remove the now useless if-clauses.

Fixes: aa73243715 ("mediatek: use CONFIG_TARGET_ROOTFS_PARTSIZE")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/mediatek/image/mt7623.mk
@@ -57,12 +57,7 @@ define Build/mt7623-mbr
 	ptgen -o $@.tmp -h 4 -s 63 -a 0 -l 1024 \
 			-t 0x41	-N uboot	-p 1M@$(UBOOT_OFFSET) \
 			-t 0xea	-N recovery	-p 40M@4M \
-		$(if $(findstring sdmmc,$1), \
-			-t 0x2e -N production	-p $(CONFIG_TARGET_ROOTFS_PARTSIZE)M@48M \
-		) \
-		$(if $(findstring emmc,$1), \
-			-t 0x2e -N production	-p $(CONFIG_TARGET_ROOTFS_PARTSIZE)M@48M \
-		)
+			-t 0x2e -N production	-p $(CONFIG_TARGET_ROOTFS_PARTSIZE)M@48M
 
 	echo -en \
 		$(if $(findstring sdmmc,$1),"SDMMC_BOOT\x00\x00\x01\x00\x00\x00\x00\x02\x00\x00") \