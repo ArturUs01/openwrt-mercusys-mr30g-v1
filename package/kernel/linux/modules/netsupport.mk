ArturUs01/openwrt-mercusys-mr30g-v1

commit bafa588d05dfd9e45ce0c223d23e5c5c64ff57d5

kernel: add mdio-bus-mux support
The MDIO bus multiplexing framework is used by some drivers
such as dwmac-sun8i.

As this is a per-driver requirement, set it to be hidden in the menu.

Signed-off-by: Mathew McBride <matt@traverse.com.au>

package/kernel/linux/modules/netsupport.mk
@@ -1363,6 +1363,21 @@ endef
 
 $(eval $(call KernelPackage,mdio))
 
+define KernelPackage/mdio-bus-mux
+  SUBMENU:=$(NETWORK_SUPPORT_MENU)
+  TITLE:=MDIO bus multiplexers
+  KCONFIG:=CONFIG_MDIO_BUS_MUX
+  HIDDEN:=1
+  FILES:=$(LINUX_DIR)/drivers/net/mdio/mdio-mux.ko
+  AUTOLOAD:=$(call AutoLoad,32,mdio-mux)
+endef
+
+define KernelPackage/mdio/description
+ Kernel framework for MDIO bus multiplexers.
+endef
+
+$(eval $(call KernelPackage,mdio-bus-mux))
+
 define KernelPackage/macsec
   SUBMENU:=$(NETWORK_SUPPORT_MENU)
   TITLE:=IEEE 802.1AE MAC-level encryption (MAC)

commit 3df3a36984fedff1abd4951f3080444d012535e2

kernel: modules: fix mdio-bus-mux description
Simple error during copy/paste

Fixes: bafa588d05df ("kernel: add mdio-bus-mux support")
Signed-off-by: Luiz Angelo Daros de Luca <luizluca@gmail.com>

package/kernel/linux/modules/netsupport.mk
@@ -1372,7 +1372,7 @@ define KernelPackage/mdio-bus-mux
   AUTOLOAD:=$(call AutoLoad,32,mdio-mux)
 endef
 
-define KernelPackage/mdio/description
+define KernelPackage/mdio-bus-mux/description
  Kernel framework for MDIO bus multiplexers.
 endef
 