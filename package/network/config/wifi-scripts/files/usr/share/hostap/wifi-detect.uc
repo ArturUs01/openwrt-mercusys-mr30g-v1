ArturUs01/openwrt-mercusys-mr30g-v1

commit 3439d8f06d3385ef8fab876eaba0abaa1fd39691

wifi-scripts: detect and configure EHT
Check if EHT/11BE supported, configure in board.json
and config/wireless.

Signed-off-by: Janusz Dziedzic <janusz.dziedzic@gmail.com>

package/network/config/wifi-scripts/files/usr/share/hostap/wifi-detect.uc
@@ -113,6 +113,7 @@ function wiphy_detect() {
 			if (band.vht_capa > 0)
 				band_info.vht = true;
 			let he_phy_cap = 0;
+			let eht_phy_cap = 0;
 
 			for (let ift in band.iftype_data) {
 				if (!ift.he_cap_phy)
@@ -120,7 +121,12 @@ function wiphy_detect() {
 
 				band_info.he = true;
 				he_phy_cap |= ift.he_cap_phy[0];
-				/* TODO: EHT */
+
+				if (!ift.eht_cap_phy)
+					continue;
+
+				band_info.eht = true;
+				eht_phy_cap |= ift.eht_cap_phy[0];
 			}
 
 			if (band_name != "2G" &&
@@ -141,13 +147,18 @@ function wiphy_detect() {
 				push(modes, "VHT20");
 			if (band_info.he)
 				push(modes, "HE20");
+			if (band_info.eht)
+				push(modes, "EHT20");
 			if (band.ht_capa & 0x2) {
 				push(modes, "HT40");
 				if (band_info.vht)
 					push(modes, "VHT40")
 			}
-			if (he_phy_cap & 0x2)
+			if (he_phy_cap & 2)
 				push(modes, "HE40");
+
+			if (eht_phy_cap && he_phy_cap & 2)
+				push(modes, "EHT40");
 
 			for (let freq in band.freqs) {
 				if (freq.disabled)
@@ -161,14 +172,26 @@ function wiphy_detect() {
 
 			if (band_name == "2G")
 				continue;
+
+			if (he_phy_cap & 4)
+				push(modes, "HE40");
+			if (eht_phy_cap && he_phy_cap & 4)
+				push(modes, "EHT40");
 			if (band_info.vht)
 				push(modes, "VHT80");
 			if (he_phy_cap & 4)
 				push(modes, "HE80");
+			if (eht_phy_cap && he_phy_cap & 4)
+				push(modes, "EHT80");
 			if ((band.vht_capa >> 2) & 0x3)
 				push(modes, "VHT160");
 			if (he_phy_cap & 0x18)
 				push(modes, "HE160");
+			if (eht_phy_cap && he_phy_cap & 0x18)
+				push(modes, "EHT160");
+
+			if (eht_phy_cap & 2)
+				push(modes, "ETH320");
 		}
 
 		let entry = wiphy_get_entry(name, path);

package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
@@ -3,7 +3,7 @@ import { readfile } from "fs";
 import * as uci from 'uci';
 
 const bands_order = [ "6G", "5G", "2G" ];
-const htmode_order = [ "HE", "VHT", "HT" ];
+const htmode_order = [ "EHT", "HE", "VHT", "HT" ];
 
 let board = json(readfile("/etc/board.json"));
 if (!board.wlan)

commit 455cd466b68928f724c8686fa06ad33e71fa3c1e

wifi-scripts: Fix typo in EHT320 name
All other places name it EHT320.

Fixes: 3439d8f06d33 ("wifi-scripts: detect and configure EHT")
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

package/network/config/wifi-scripts/files/usr/share/hostap/wifi-detect.uc
@@ -191,7 +191,7 @@ function wiphy_detect() {
 				push(modes, "EHT160");
 
 			if (eht_phy_cap & 2)
-				push(modes, "ETH320");
+				push(modes, "EHT320");
 		}
 
 		let entry = wiphy_get_entry(name, path);