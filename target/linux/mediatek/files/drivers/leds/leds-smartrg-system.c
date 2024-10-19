neheb/openwrt

mediatek: leds-smartrg: use devm_mutex_init
No longer need normal _remove function. Replaced with _disable.

Prepare for newer kernel version by using remove_new. In newer kernels,
it goes back to remove but remove_new is available with both 6.6 and
6.12.

Signed-off-by: Rosen Penev <rosenp@gmail.com>

target/linux/mediatek/files/drivers/leds/leds-smartrg-system.c
@@ -163,6 +163,7 @@ srg_led_probe(struct i2c_client *client)
 {
 	struct device_node *np = client->dev.of_node, *child;
 	struct srg_led_ctrl *sysled_ctrl;
+	int err;
 
 	sysled_ctrl = devm_kzalloc(&client->dev, sizeof(*sysled_ctrl), GFP_KERNEL);
 	if (!sysled_ctrl)
@@ -170,7 +171,9 @@ srg_led_probe(struct i2c_client *client)
 
 	sysled_ctrl->client = client;
 
-	mutex_init(&sysled_ctrl->lock);
+	err = devm_mutex_init(&client->dev, &sysled_ctrl->lock);
+	if (err)
+		return err;
 
 	i2c_set_clientdata(client, sysled_ctrl);
 
@@ -193,15 +196,6 @@ static void srg_led_disable(struct i2c_client *client)
 		srg_led_i2c_write(sysled_ctrl, i, 0);
 }
 
-static void
-srg_led_remove(struct i2c_client *client)
-{
-	struct srg_led_ctrl *sysled_ctrl = i2c_get_clientdata(client);
-
-	srg_led_disable(client);
-	mutex_destroy(&sysled_ctrl->lock);
-}
-
 static const struct i2c_device_id srg_led_id[] = {
 	{ "srg-sysled", 0 },
 	{ }
@@ -220,7 +214,7 @@ static struct i2c_driver srg_sysled_driver = {
 		.of_match_table = of_srg_led_match,
 	},
 	.probe		= srg_led_probe,
-	.remove		= srg_led_remove,
+	.remove_new	= srg_led_disable,
 	.id_table	= srg_led_id,
 };
 module_i2c_driver(srg_sysled_driver);

ArturUs01/openwrt-mercusys-mr30g-v1

Link: https://github.com/openwrt/openwrt/pull/16651
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

target/linux/mediatek/files/drivers/leds/leds-smartrg-system.c
@@ -163,6 +163,7 @@ srg_led_probe(struct i2c_client *client)
 {
 	struct device_node *np = client->dev.of_node, *child;
 	struct srg_led_ctrl *sysled_ctrl;
+	int err;
 
 	sysled_ctrl = devm_kzalloc(&client->dev, sizeof(*sysled_ctrl), GFP_KERNEL);
 	if (!sysled_ctrl)
@@ -170,7 +171,9 @@ srg_led_probe(struct i2c_client *client)
 
 	sysled_ctrl->client = client;
 
-	mutex_init(&sysled_ctrl->lock);
+	err = devm_mutex_init(&client->dev, &sysled_ctrl->lock);
+	if (err)
+		return err;
 
 	i2c_set_clientdata(client, sysled_ctrl);
 
@@ -193,15 +196,6 @@ static void srg_led_disable(struct i2c_client *client)
 		srg_led_i2c_write(sysled_ctrl, i, 0);
 }
 
-static void
-srg_led_remove(struct i2c_client *client)
-{
-	struct srg_led_ctrl *sysled_ctrl = i2c_get_clientdata(client);
-
-	srg_led_disable(client);
-	mutex_destroy(&sysled_ctrl->lock);
-}
-
 static const struct i2c_device_id srg_led_id[] = {
 	{ "srg-sysled", 0 },
 	{ }
@@ -220,7 +214,7 @@ static struct i2c_driver srg_sysled_driver = {
 		.of_match_table = of_srg_led_match,
 	},
 	.probe		= srg_led_probe,
-	.remove		= srg_led_remove,
+	.remove		= srg_led_disable,
 	.id_table	= srg_led_id,
 };
 module_i2c_driver(srg_sysled_driver);